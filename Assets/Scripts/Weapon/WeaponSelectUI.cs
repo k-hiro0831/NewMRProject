237b80434df02dc9667f29f04283fc27a8d7f552
kimura <arumik0831@gmail.com>
2022-01-31 14:22:15 +0900

    UI追従

---
+++ Assets/Scripts/Weapon/WeaponSelectUI.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponSelectUI : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _selectUI = null;
+
+    private GameObject _selectParent = null;
+
+    private void Start()
+    {
+        _selectParent = this.gameObject;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.O))
+        {
+            OpenSelectUI();
+        }
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            CloseSelectUI();
+        }
+    }
+
+    /// <summary>
+    /// •Šíw“ü‰æ–Ê‚ğŠJ‚­
+    /// </summary>
+    public void OpenSelectUI()
+    {
+        _selectUI.SetActive(true);
+    }
+    /// <summary>
+    /// •Šíw“ü‰æ–Ê‚ğ•Â‚¶‚é
+    /// </summary>
+    public void CloseSelectUI()
+    {
+        _selectUI.SetActive(false);
+    }
+
+}
